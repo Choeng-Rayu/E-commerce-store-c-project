dbd731a833d7b4dd0ff6289649ec64163547696c
Choeng-Rayu <choengrayu307@gmail.com>
2024-12-12 13:43:46 +0700

    Add files via upload

---
+++ e-commerce.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+#define FILE_HISTORY "history data.csv"
+#define FILE_INVOICE "Invoice data.csv"
+#define FILE_PRODUCT "product data.csv"
+
+struct Node
+{
+    Node* next;
+    string name;
+    int id;
+    string gender;
+    int amount;
+    int age;
+
+};
+class Customer_queue
+{
+    int amount_customer; //size
+    Node* front;
+    Node* reare;
+public:
+    Customer_queue()
+    {
+        amount_customer = 0;
+        front = NULL;
+        reare = NULL;
+    }
+
+
+};
+
+class Seller
+{
+    int amount_product; //
+    Node* head;
+    Node* tail;
+    Seller()
+    {
+        head = NULL;
+        tail = NULL;
+        amount_product = 0;
+    }
+
+
+};
+
+int main ()
+{
+
+    return 0;
+}
+
+
