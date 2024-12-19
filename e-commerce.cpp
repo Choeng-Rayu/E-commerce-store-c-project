3785547e81c20aba0bbc7a952d617e431aa29c3a
Choeng-Rayu <choengrayu307@gmail.com>
2024-12-19 14:33:21 +0700

    2:33

--- e-commerce.cpp
+++ e-commerce.cpp
@@ -1,4 +1,9 @@
 #include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<iomanip>
+#include<windows.h>
 
 using namespace std;
 
@@ -8,6 +13,7 @@ using namespace std;
 #define FILE_PERSONAL "personal customer.csv"
 #define FILE_USER_LOGIN "user login.csv"
 #define FILE_SELLER_LOGIN "seller login.csv"
+#define FILE_Add_TO_CART "add to cart.csv"
 
 
 //Note read file for ELITE 
@@ -37,8 +43,10 @@ struct Login
 {
     string usernameCustomer;
     string pwCustomer;
+    string emailCustomer;
     string usernameSeller;
     string pwseller;
+    string emailSeller;
 };
 
 struct Date
@@ -66,6 +74,73 @@ public:
 
 };
 
+class File_handling
+{
+    void readFile_history(){
+        fstream file_history(FILE_HISTORY, ios::in);
+        //code here
+    }
+    void writeFile_history(){
+        fstream file_history(FILE_HISTORY, ios::out);
+
+    }
+
+
+    void readFile_invoice(){
+        fstream file_invoice(FILE_INVOICE, ios::in);
+        //code here
+    }
+    void writeFile_history(){
+        fstream file_invoice(FILE_INVOICE, ios::out);
+
+    }
+
+    void readFile_personal(){
+        fstream file_persona;(FILE_PERSONAL, ios::in);
+        //code here
+    }
+    void writeFile_personal(){
+        fstream file_personal(FILE_PERSONAL, ios::out);
+
+    }
+
+    void readFile_product(){
+        fstream file_product(FILE_PRODUCT, ios::in);
+        //code here
+    }
+    void writeFile_product(){
+        fstream file_product(FILE_PRODUCT, ios::out);
+
+    }
+
+    void readFile_seller_login(){
+        fstream file_seller(FILE_SELLER_LOGIN, ios::in);
+        //code here
+    }
+    void writeFile_seller_login(){
+        fstream file_seller(FILE_SELLER_LOGIN, ios::out);
+
+    }
+
+    void readFile_user_login(){
+        fstream file_user(FILE_USER_LOGIN, ios::in);
+        //code here
+    }
+    void writeFile_user_login(){
+        fstream file_user(FILE_USER_LOGIN, ios::out);
+
+    }
+
+    void readFile_add_to_cart(){
+        fstream file_add_to_cart(FILE_Add_TO_CART, ios::in);
+        //code here
+    }
+    void writeFile_add_to_cart(){
+        fstream file_add_to_cart(FILE_Add_TO_CART, ios::out);
+
+    }
+};
+
 class Seller
 {
     int amount_product; //
@@ -77,16 +152,41 @@ class Seller
         amount_product = 0;
     }
 };
-class AddToCard
+class CRUD
+{
+    Node* head;
+    Node* tail;
+    int quantity;
+    CRUD(){
+        head = NULL;
+        tail = NULL;
+        quantity = 0;
+    }
+};
+class AddToCart
 {
     Node* top;
     int totalProduct;
-    AddToCard(){
+    AddToCart(){
         top = NULL;
 
     }
 
 };
+
+
+class CustomerLogin
+{
+    Login cusotmer;
+
+
+};
+class SellerLogin
+{
+    Login seller;
+
+};
+
 
 int main ()
 {
