c2f012e2aced35c88deeeb36612f63e5059473ec
Choeng-Rayu <choengrayu307@gmail.com>
2024-12-22 23:51:29 +0700

    change yu

--- e-commerce new.cpp
+++ e-commerce new.cpp
@@ -98,7 +98,7 @@ class date_handling
         date.month = 1 + ltm->tm_mon;
         date.days = ltm->tm_mday;
     }
-    
+
 };
 
 class Admine
@@ -112,7 +112,7 @@ class customer_account
     Node* head;
     Node* tail;
 
-public:
+    public:
     customer_account() {
         total_customer = 0;
         head = nullptr;
@@ -128,8 +128,7 @@ public:
             delete toDelete;
         }
     }
-
-    void insert_customer() {
+    void add_customer_input_data(){
         string name, email, phone, password;
         int money, id;
 
@@ -153,6 +152,10 @@ public:
         cout << "Enter your password: ";
         cin.ignore();
         getline(cin, password);
+        insert_customer(name, email, phone, password, id, money);
+
+    }
+    void insert_customer(string name, string email, string phone,string password,  int id, int money) { 
 
         Node* newCustomer = new Node;
         newCustomer->customer.name_customer = name;
@@ -171,30 +174,180 @@ public:
         }
         total_customer++;
     }
+    void load_data_from_file(){
+        fstream file(FILE_PERSONAL, ios::in);
+        if (!file.is_open()) return;
+        string name, email, phone, password;
+        int money, id;
+        string line;
+        while (getline(file, line))
+        {
+            stringstream ss(line);
+            getline(ss, name, ',');
+            getline(ss, email, ',');
+            getline(ss, phone, ',');
+            getline(ss, password, ',');
+            ss >> id;
+            ss.ignore();
+            ss >> money;
+            insert_customer(name, email, phone, password, id, money);
+        }
+        file.close();
+    }
+
+    void save_data_to_file(){
+        fstream file(FILE_PERSONAL, ios::out);
+        if (!file.is_open()) return;
+        Node* current = head;
+        while (current)
+        {
+            file << current->customer.name_customer << ","
+                 << current->customer.email_customer << ","
+                 << current->customer.phone_customer << ","
+                 << current->customer.password_customer << ","
+                 << current->customer.id_customer << ","
+                 << current->customer.money_customer << endl;
+            current = current->next;
+        }
+        cout << "Data saved to file successfully!" << endl;
+        file.close();
+    }
+
     bool check_customer_is_empty(){
         if(total_customer == 0){
             return true;
         }
         return false;
     }
-    void delete_customer_position(){
+
+    void delete_customer_position() {
         string name;
         string password;
-        if(check_customer_is_empty()){
+
+        if (check_customer_is_empty()) {
             cout << "There is no customer account" << endl;
             return;
         }
+
         cout << "Enter Customer name you want to delete: ";
         cin.ignore();
         getline(cin, name);
-        cout << "Enter Password: " ;
-        cin.ignore();
+        cout << "Enter Password: ";
         getline(cin, password);
-        Node* delete_customer;
+
+        Node* delete_customer = nullptr;
         Node* current = head;
         Node* previous = nullptr;
 
+        while (current != nullptr) {
+            if (current->customer.name_customer == name && current->customer.password_customer == password) {
+                
+                delete_customer = current;
+
+                // Display customer details
+                cout << "Customer details:" << endl;
+                cout << "Name: " << current->customer.name_customer << endl
+                    << "ID: " << current->customer.id_customer << endl
+                    << "Email: " << current->customer.email_customer << endl
+                    << "Phone: " << current->customer.phone_customer << endl
+                    << "Money: " << current->customer.money_customer << endl
+                    << "Password: " << current->customer.password_customer << endl;
+
+                cout << "Please press Enter to delete..." << endl;
+                cin.ignore();
+
+                // Adjust pointers to delete the node
+                if (current == head) {
+                    head = head->next;
+                    if (head == nullptr) tail = nullptr; // Handle single-element list
+                } else if (current == tail) {
+                    tail = previous;
+                    tail->next = nullptr;
+                } else {
+                    previous->next = current->next;
+                }
+
+                delete delete_customer;
+                total_customer--;
+                cout << "Customer deleted successfully!" << endl;
+                return;
+            }
+
+            previous = current;
+            current = current->next;
+        }
+
+        cout << "Customer not found!" << endl;
     }
+
+    void update_customer(){
+        string name;
+        string password;
+
+        if (check_customer_is_empty()) {
+            cout << "There is no customer account" << endl;
+            return;
+        }
+
+        cout << "Enter Customer name you want to delete: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Enter Password: ";
+        getline(cin, password);
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->customer.name_customer == name && current->customer.password_customer == password) {
+                // Display customer details
+                cout << "Customer details:" << endl;
+                cout << "Name: " << current->customer.name_customer << endl
+                    << "ID: " << current->customer.id_customer << endl
+                    << "Email: " << current->customer.email_customer << endl
+                    << "Phone: " << current->customer.phone_customer << endl
+                    << "Money: " << current->customer.money_customer << endl
+                    << "Password: " << current->customer.password_customer << endl;
+                cout << "Enter new name: ";
+                cin.ignore();
+                getline(cin, current->customer.name_customer);
+                cout << "Enter new email: ";
+                cin >> current->customer.email_customer;
+                cout << "Enter new phone: ";
+                cin.ignore();
+                getline(cin, current->customer.phone_customer);
+                cout << "Enter new money: ";
+                cin >>current->customer.money_customer;
+                cout << "Enter new Id:";
+                cin >> current->customer.id_customer;
+                cout << "Enter new password: ";
+                cin.ignore();
+                getline(cin, current->customer.password_customer);
+                cout << "Customer updated successfully!" << endl;
+                break;
+            }
+            current = current->next;
+        }
+
+        cout << "Customer not found!" << endl;
+    }
+    int get_total_customer(){
+        return total_customer;
+    }
+    void display_customer(){
+        if (check_customer_is_empty()) {
+            cout << "There is no customer account" << endl;
+            return;
+        }
+        Node* current = head;
+        while (current != nullptr) {
+            cout << "Name: " << current->customer.name_customer << endl
+                << "ID: " << current->customer.id_customer << endl
+                << "Email: " << current->customer.email_customer << endl
+                << "Phone: " << current->customer.phone_customer << endl
+                << "Money: " << current->customer.money_customer << endl
+                << "Password: " << current->customer.password_customer << endl;
+            current = current->next;
+        }
+    }
+
 };
 
 
@@ -212,6 +365,191 @@ class seller_account
     ~seller_account(){
 
         //code here
+
+    }
+    void add_new_seller_input_data(){
+        string name_seller, email_seller, pw_seller, phone_seller; 
+        int id_seller, seller_money; 
+        cout << "Enter seller name: ";
+        cin.ignore();
+        getline(cin, name_seller);
+        cout << "Enter seller email: ";
+        cin >> email_seller;
+        cout << "Enter seller phone: ";
+        cin.ignore();
+        getline(cin, phone_seller);
+        cout << "Enter seller id: ";
+        cin >> id_seller;
+        cout << "Enter money: ";
+        cin >> seller_money;
+        cout << "Enter seller password: ";
+        cin.ignore();
+        getline(cin, pw_seller);
+        insert_new_seller_account(name_seller, email_seller, phone_seller, pw_seller, id_seller, seller_money);
+    }
+    void insert_new_seller_account(string name_seller, string email_seller, string phone_seller, string pw_seller, int id_seller, int seller_money){
+        Node* newSeller = new Node;
+        newSeller->seller.username_seller = name_seller;
+        newSeller->seller.email_seller = email_seller;
+        newSeller->seller.password_seller = pw_seller;
+        newSeller->next = NULL;
+        if (total_account_seller == 0)
+        {
+            head = tail = newSeller;
+        }
+        else
+        {
+            tail->next = newSeller;
+            tail = newSeller;
+        }
+        total_account_seller++;
+    }
+    void load_data_seller_from_file(){
+        fstream file(FILE_PERSONAL_SELLER, ios::in);
+        if (!file.is_open()) return;
+        string name, email, phone, password;
+        int money, id;
+        string line;
+        while (getline(file, line))
+        {
+            stringstream ss(line);
+            getline(ss, name, ',');
+            getline(ss, email, ',');
+            getline(ss, phone, ',');
+            getline(ss, password, ',');
+            ss >> id;
+            ss.ignore();
+            ss >> money;
+            insert_new_seller_account(name, email, phone, password, id, money);
+        }
+        file.close();
+    }
+    void save_data_seller_to_file(){
+        fstream file(FILE_PERSONAL_SELLER, ios::out);
+        if (!file.is_open()) return;
+        Node* current = head;
+        while (current)
+        {
+            file << current->seller.username_seller << ","
+                 << current->seller.email_seller << ","
+                 << current->seller.phone_seller << ","
+                 << current->seller.password_seller << ","
+                 << current->seller.id_seller << ","
+                 << current->seller.money_seller << endl;
+            current = current->next;
+        }
+        file.close();
+    }
+
+    bool check_seller_is_empty(){
+        if(total_account_seller == 0){
+            return true;
+        }
+        return false;
+    }
+    void delete_seller_position(){
+        string name_seller;
+        string password_seller;
+        if (check_seller_is_empty())
+        {
+            cout << "There is no seller account" << endl;
+            return;
+        }
+        cout << "Enter Seller name you want to delete: ";
+        cin.ignore();
+        getline(cin, name_seller);
+        cout << "Enter Password: ";
+        getline(cin, password_seller);
+        Node* delete_seller = nullptr;
+        Node* current = head;
+        Node* previous = nullptr;
+        while (current != nullptr)
+        {
+            if (current->seller.username_seller == name_seller && current->seller.password_seller == password_seller)
+            {
+                delete_seller = current;
+                cout << "Seller details:" << endl;
+                cout << "Name: " << current->seller.username_seller << endl
+                    << "ID: " << current->seller.id_seller << endl
+                    << "Email: " << current->seller.email_seller << endl
+                    << "Phone: " << current->seller.phone_seller << endl
+                    << "Money: " << current->seller.money_seller << endl
+                    << "Password: " << current->seller.password_seller << endl;
+                cout << "Please press Enter to delete..." << endl;
+                cin.ignore();
+                if (current == head)
+                {
+                    head = head->next;
+                    if (head == nullptr) tail = nullptr;
+                }
+                else if (current == tail)
+                {
+                    tail = previous;
+                    tail->next = nullptr;
+                }
+                else
+                {
+                    previous->next = current->next;
+                }
+                delete delete_seller;
+                total_account_seller--;
+                cout << "Seller deleted successfully!" << endl;
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+        cout << "Seller not found!" << endl;
+    }
+    void update_seller(){
+        string name_seller;
+        string password_seller;
+        if (check_seller_is_empty())
+        {
+            cout << "There is no seller account" << endl;
+            return;
+        }
+        cout << "Enter Seller name you want to delete: ";
+        cin.ignore();
+        getline(cin, name_seller);
+        cout << "Enter Password: ";
+        getline(cin, password_seller);
+        Node* current = head;
+        while (current != nullptr)
+        {
+            if (current->seller.username_seller == name_seller && current->seller.password_seller == password_seller)
+            {
+                cout << "Seller details:" << endl;
+                cout << "Name: " << current->seller.username_seller << endl
+                    << "ID: " << current->seller.id_seller << endl
+                    << "Email: " << current->seller.email_seller << endl
+                    << "Phone: " << current->seller.phone_seller << endl
+                    << "Money: " << current->seller.money_seller << endl
+                    << "Password: " << current->seller.password_seller << endl;
+                cout << "Enter new name: ";
+                cin.ignore();
+                getline(cin, current->seller.username_seller);
+                cout << "Enter new email: ";
+                cin >> current->seller.email_seller;
+                cout << "Enter new phone: ";
+                cin.ignore();
+                getline(cin, current->seller.phone_seller);
+                cout << "Enter new money: ";
+                cin >> current->seller.money_seller;
+                cout << "Enter new Id:";
+                cin >> current->seller.id_seller;
+                cout << "Enter new password: ";
+                cin.ignore();
+                getline(cin, current->seller.password_seller);
+                cout << "Seller updated successfully!" << endl;
+                break;
+            }
+            current = current->next;
+        }
+        cout << "Seller not found!" << endl;
+    }
+    int get_total_seller(){
+        return total_account_seller;
     }
 
 };
@@ -264,11 +602,11 @@ class add_to_cart
 
 };
 
-class 
-
-
 int main ()
 {
-
+    customer_account customer;
+    seller_account seller;
+    customer.load_data_from_file();
+    customer.display_customer();
     return 0;
 }
